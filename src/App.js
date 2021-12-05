47399e2b2979211ce9a1d6aa55977006428a3870
elina <elinapark0818@gmail.com>
2021-12-05 23:35:18 +0900

    Make getRandomIndex

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Props from "./components/props";
 
 function App() {
+  const ramen = [
+    "까르보불닭",
+    "진순이",
+    "너구리",
+    "짜파게티",
+    "간짬뽕",
+    "신라면",
+  ];
+
+  const getRandomIndex = (length) => {
+    return parseInt(Math.random() * length);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        내가 지금 먹고 싶은 라면은? {ramen[getRandomIndex(ramen.length)]}
       </header>
+      <main>
+        <Props />
+      </main>
     </div>
   );
 }
