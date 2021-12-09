e79a966cf3cd36bf3685919bdb90309c6beb844a
elina <elinapark0818@gmail.com>
2021-12-09 09:31:49 +0900

    Template Reset

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Props from "./components/props";
+import React from "react";
 
 function App() {
-  const ramen = [
-    "까르보불닭볶음면",
-    "진순이",
-    "너구리",
-    "짜파게티",
-    "간짬뽕",
-    "신라면",
-    "틈새라면",
-    "치즈볶이",
-    "열라면",
-  ];
-
-  const getRandomIndex = (length) => {
-    return parseInt(Math.random() * length);
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        내가 지금 먹고 싶은 라면은? {ramen[getRandomIndex(ramen.length)]}
-      </header>
-      <main>
-        <Props />
-      </main>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
